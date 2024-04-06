5c6058467c49a868c118296f43d56f348ab8ab57
discphy <dinophy@nate.com>
2024-04-06 12:41:35 +0900

    STEP 1-1 문자찾기

---
+++ src/v1/FindString.java
@@ -0,0 +1,29 @@
+package v1;
+
+import java.util.Scanner;
+
+public class FindString {
+
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        for (char x : str.toCharArray()) {
+            if (x == t) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindString T = new FindString();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char t = kb.next().charAt(0);
+
+        System.out.println(T.solution(str, t));
+    }
+}
