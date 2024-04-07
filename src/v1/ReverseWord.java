c44e05c774137c6185a880a24b6ba0decf57b102
discphy <dinophy@nate.com>
2024-04-07 13:08:59 +0900

    STEP 1-3 단어 뒤집기

---
+++ src/v1/ReverseWord.java
@@ -0,0 +1,51 @@
+package v1;
+
+import java.util.*;
+
+public class ReverseWord {
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        /*for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }*/
+
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length() - 1;
+
+            // TODO 중요
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ReverseWord T = new ReverseWord();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+}
