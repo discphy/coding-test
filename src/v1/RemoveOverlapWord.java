47c4eb8a728ba65002430e29d2d6c0c0b74f4818
discphy <dinophy@nate.com>
2024-04-10 12:12:05 +0900

    STEP 1-6 중복문자제거

---
+++ src/v1/RemoveOverlapWord.java
@@ -0,0 +1,27 @@
+package v1;
+
+import java.util.Scanner;
+
+public class RemoveOverlapWord {
+
+    public String solution(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i == str.indexOf(str.charAt(i))) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RemoveOverlapWord T = new RemoveOverlapWord();
+        Scanner kb = new Scanner(System.in);
+
+        String str = kb.next();
+
+        System.out.println(T.solution(str));
+    }
+}
