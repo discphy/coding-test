15df0a7975181a32f3df1148b6f5de11399b4b99
discphy <dinophy@nate.com>
2024-04-15 08:17:39 +0900

    STEP 1-9 숫자만 추출

---
+++ src/v1/ExtractNumber.java
@@ -0,0 +1,37 @@
+package v1;
+
+import java.util.Scanner;
+
+public class ExtractNumber {
+
+    public int solution(String str) {
+        int answer = 0;
+
+        for (char x : str.toCharArray()) {
+            if (x >= 48 && x <= 57) {
+                answer = answer * 10 + (x - 48);
+            }
+        }
+
+        return answer;
+    }
+
+    public int solution2(String str) {
+        String answer = "";
+
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) answer += x;
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        ExtractNumber T = new ExtractNumber();
+        Scanner kb = new Scanner(System.in);
+
+        String str = kb.next();
+
+        System.out.println(T.solution2(str));
+    }
+}
