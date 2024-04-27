e57a7bfdb90a71e7fd1ca49effcffc7f24a215f2
discphy <dinophy@nate.com>
2024-04-27 11:13:04 +0900

    Test main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+  public String solution(int cnt, String str) {
+    String answer = "";
+
+	for (int i = 0; i < cnt; i++) {
+     	String tmp = str.substring(0, 7).replace("#", "1").replace("*", "0");
+
+      	answer += (char) Integer.parseInt(tmp, 2);
+
+        str = str.substring(7);
     }
+
+      return answer;
+  }
+
+  public static void main(String[] args){
+    Main main = new Main();
+    Scanner in=new Scanner(System.in);
+    int input1 = in.nextInt();
+    String input2 = in.next();
+
+    System.out.println(main.solution(input1, input2));
+    return ;
+  }
 }
