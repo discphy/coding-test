bca08c4f67ef5c430adf301925ad81da8239a54a
discphy <dinophy@nate.com>
2024-05-21 08:13:46 +0900

    STEP 3-5 (1) recap

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
-    public List<Integer> solution(int n, int[] arr1, int m, int[] arr2) {
-        List<Integer> answer = new ArrayList<>();
+    public int solution(int n) {
+        int answer = 0;
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        int cnt = 1;
+        n--;
 
-        int p1 = 0;
-        int p2 = 0;
+        while (n > 0) {
+            cnt++;
+            n -= cnt;
 
-        while (p1 < n && p2 < m) {
-            if (arr1[p1] < arr2[p2]) {
-                p1++;
-            } else if (arr1[p1] > arr2[p2]) {
-                p2++;
-            } else {
-                answer.add(arr1[p1]);
-                p1++;
-                p2++;
+            if (n % cnt == 0) {
+                answer++;
             }
         }
-
 
         return answer;
     }
@@ -31,22 +24,7 @@ public class Main {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
         int input1 = in.nextInt();
-        int[] arr1 = new int[input1];
 
-        for (int i = 0; i < input1; i++) {
-            arr1[i] = in.nextInt();
-        }
-
-        int input2 = in.nextInt();
-        int[] arr2 = new int[input2];
-
-        for (int i = 0; i < input1; i++) {
-            arr2[i] = in.nextInt();
-        }
-
-
-        for (int i : T.solution(input1, arr1, input2, arr2)) {
-            System.out.print(i + " ");
-        }
+        System.out.print(T.solution(input1));
     }
 }
