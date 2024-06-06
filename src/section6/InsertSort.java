f339649826e1ade2ed65b987680a98b5194f6153
discphy <dinophy@nate.com>
2024-06-07 08:20:18 +0900

    STEP 6-3 삽입정렬

--- src/section6/InsertSort.java
+++ src/section6/InsertSort.java
@@ -1,29 +1,30 @@
 package section6;
 
 import java.util.Scanner;
-import java.util.Stack;
 
-// 6-1 선택정렬
-public class SelectedArray {
+// 6-3 삽입정렬
+public class InsertSort {
 
     public int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n; i++) {
-            int idx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[idx]) {
-                    idx = j;
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i], j;
+
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > temp) {
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = temp;
+
+            arr[j + 1] = temp;
         }
 
         return arr;
     }
 
     public static void main(String[] args) {
-        SelectedArray T = new SelectedArray();
+        InsertSort T = new InsertSort();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
