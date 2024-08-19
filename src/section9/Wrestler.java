db414709303bae18e305c2d2298f65cda0405d6a
discphy <dinophy@nate.com>
2024-08-20 08:28:25 +0900

    STEP 9-1 씨름선수

---
+++ src/section9/Wrestler.java
@@ -0,0 +1,57 @@
+package section9;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// 9-1 씨름선수
+public class Wrestler {
+
+    static class Body implements Comparable<Body> {
+
+        public int h, w;
+
+        public Body(int h, int w) {
+            this.h = h;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Body o) {
+            return o.h - this.h;
+        }
+    }
+
+    public int solution(ArrayList<Body> arr, int n) {
+        int cnt = 0;
+
+        Collections.sort(arr);
+
+        int max = Integer.MIN_VALUE;
+
+        for (Body ob : arr) {
+            if (ob.w > max) {
+                max = ob.w;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Wrestler T = new Wrestler();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        ArrayList<Body> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int h = kb.nextInt();
+            int w = kb.nextInt();
+            arr.add(new Body(h, w));
+        }
+
+        System.out.println(T.solution(arr, n));
+    }
+}
